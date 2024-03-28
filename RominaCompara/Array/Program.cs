4c4d103bd431db842f9ed432200f78d62763e9a9
romilarita85 <romilarita85@hotmail.com>
2024-03-28 11:28:34 -0300

    repaso array

--- RominaCompara/Array/Program.cs
+++ RominaCompara/Array/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Array
+﻿//ARRAY: Cadena o conjunto de  datos del mismo tipo
+//Se reserva espacios consecutivos del tamaño de ese mensaje
+//[] -> para identificar el tipo de dato
+
+namespace Array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
