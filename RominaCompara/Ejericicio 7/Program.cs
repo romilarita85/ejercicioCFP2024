5cf165d71d124d1b5f1f616506f21a860709dbaa
romilarita85 <romilarita85@hotmail.com>
2024-03-29 15:08:09 -0300

    ejercicios

--- RominaCompara/Ejericicio 7/Program.cs
+++ RominaCompara/Ejericicio 7/Program.cs
@@ -12,6 +12,8 @@
 //j.	El número máximo
 //k.	De los negativos el mínimo
 
+using System.ComponentModel.Design;
+
 namespace Ejericicio_7
     
 {
@@ -29,101 +31,80 @@ namespace Ejericicio_7
             int cantidadPositivo = 0;//Definir cantidadPositivo Como Entero;
             int cantidadCeros = 0;//Definir cantidadCeros Como Entero;
             int cantidadPares = 0;//Definir cantidadPares Como Entero;
-            int promedioNegativo;//Definir promedioNegativo Como Real;
-            int promedioPositivo;//Definir promedioPositivo Como Real;
+            decimal promedioNegativo;//Definir promedioNegativo Como Real;
+            decimal promedioPositivo;//Definir promedioPositivo Como Real;
             int diferenciaEntreNumeros;//Definir diferenciaEntreNumeros Como Entero;
             int numeroMaximo;//Definir numeroMaximo Como Entero;
-            int numeroMiminoNegativo;//Definir numeroMinimoNegativo Como Entero;
-            String banderaPrimerNegativo = "vacio";//Definir banderaPrimerNegativo Como caracter;
-       
-            for (i=0;i >repeticiones; i++)
+            int numeroMinNegativo;//Definir numeroMinimoNegativo Como Entero;
+            String banderaPrimerNegativo = "falso";//Definir banderaPrimerNegativo Como caracter;
+
+            for (i = 1; i > repeticiones; i++) 
             {
                 Console.WriteLine($"Ingrese el numero {i} de  {repeticiones}");
-                //Console.int.Parse(numeroIngresado);
-            }
-            if (numeroIngresado > 0) //todos los positivos
-            {
-                sumaPositivo = sumaPositivo + numeroIngresado; // acumulador
-                cantidadPositivo = cantidadPositivo + 1; // contador siempre suma un numero fijo y constante
-            }
-            else 
-            {
-                if (numeroIngresado < 0) //todos los negativos
+                numeroIngresado = int.Parse(Console.ReadLine());
+                if (numeroIngresado > 0)//todos los positivos
                 {
-                    sumaNegativo = sumaNegativo + numeroIngresado;
+                    sumaPositivo = sumaPositivo + numeroIngresado; // acumulador
+                    cantidadPositivo = cantidadPositivo + 1; // contador siempre suma un numero fijo y constante
+
                 }
-                cantidadNegativo = cantidadNegativo + 1;
-                ////Para hacer el punto ->(k-De los negativos el mínimo)
-                //si cantidadNegativo == 1 O numeroIngresados < numeroMinimoNegativo Entonces //cantidadNegativo (contador) actua como variable de control
-                //					numeroMinimoNegativo = numeroIngresados;
+                else 
+                {
+                    if (numeroIngresado < 0) //todos los negativos
+                    {
+                        sumaNegativo = sumaNegativo + numeroIngresado;
+                    }
+                    else //va el cero
+                    {
+                        cantidadNegativo = cantidadNegativo + 1;
+                    }
 
-                 //FinSi
+                }
+                if(numeroIngresado % 2 == 0 && numeroIngresado<> 0)//f.Cantidad de numeros pares /queremos que sean distintos de cero
+                {
+                    cantidadPares = cantidadPares + 1; 
+                }
 
+                //j.El número máximo
+                //numero maximo, en este caso "i" (variable de control me indica cual es el primer numero ingresado)
+                if (i == 1 O numeroIngresado > numeroMaximo)
+                {
+                    numeroMaximo = numeroIngresado;
+                }
+
+                if(numeroIngresado < 0 && banderaPrimerNegativo == Falso)||(numeroIngresado < 0 && numeroIngresado < numeroMinNegativo)
+                {
+                //aca ingresa el primer numero negativo
+                //declare arriba que bandera estaba vacia (falso)- 
+                //se cambia el valor de vacio a lleno (verdadero)-adentro se declara por el valor contrario y pasa a "lleno" 
+                    numeroMinNegativo = numeroIngresado;
+                    banderaPrimerNegativo = Verdadero; //invierto la bandera por que ya se cumplio ya ingreso el primer numero- cambio el estado de la bandera
+                                                   //la bandera en el proximo caso estaria llena.
+                }
             }
-            Console.WriteLine($"Valor del control despues del bucle {i}");
-
         }
     }
 }
-
 	
+	if (cantidadPositivo<>0)
+    {
+        promedioPositivo = sumaPositivo / cantidadPositivo;// en el promedio = el dividendo nunca puede ser cero
+    }
+    else
+    {
+        promedioPositivo = 0;
 
+    }
 
+//h.Promedio de negativos.
+if(cantidadNegativo <> 0)
+{ 
+     promedioNegativo = sumaNegativo / cantidadNegativo;
+}
+else 
+{ 
+     medioNegativo = 0;
+}
 
-//		si numeroIngresados % 2 == 0 Y numeroIngresados <> 0 Entonces //numeros pares
-//			cantidadPares = cantidadPares + 1;
-//FinSi
-
-//// El numero maximo , en este caso i (variable de control me indica cual es el primer numero ingresado)
-
-//si i == 1 O numeroIngresados > numeroMaximo Entonces
-//			numeroMaximo = numeroIngresados;
-
-//FinSi
-
-////De los negativos el mínimo
-
-////si (numeroIngresados < 0 Y banderaPrimerNegativo =="vacio") O (numeroIngresados < 0 Y numeroIngresados<numeroMinimoNegativo) Entonces 
-////aca ingresa el primer numero negativo para eso declaro una bancera, se utiliza siempre para comparar
-
-////numeroMinimoNegativo = numeroIngresados;
-////banderaPrimerNegativo ="lleno"; 
-
-
-////FinSi
-
-//Fin Para
-	
-//	//PROMEDIOS: se calculan por fuera del bucle repetitivo salvo excepciones
-	
-//	si cantidadPositivo <> 0 Entonces //el promedio es importante que no se divida por cero
-//		promedioPositivo = sumaPositivo /cantidadPositivo;
-//Imprimir "El promedio de los numeros positivos es : ", promedioPositivo;
-//SiNo
-//    promedioPositivo = 0;
-//Imprimir "El promedio de los numeros positivos no se puede mostrar dado que no se ingresaron positivos ";
-//FinSi
-
-//si cantidadNegativo <> 0 Entonces //el promedio es importante que no se divida por cero
-//		promedioNegativo = sumaNegativo / cantidadNegativo;
-//Imprimir "El minimo de los numeros negativos es : ", numeroMinimoNegativo; //k-De los minimos el negativo
-//Imprimir "El promedio de los numeros negativos es : ", promedioNegativo;
-//SiNo
-//    promedioNegativo = 0;
-//Imprimir "El promedio y el minimo de los numeros negativos no se puede mostrar dado que no se ingresaron negativos ";
-//FinSi
-
-////Diferencia entre positivos y negativos, (positivos-negativos). 
-//diferenciaEntreNumeros = sumaPositivo + sumaNegativo; //le descuento los negativos
-
-//Imprimir "El valor de la suma de los negativos es : ", sumaNegativo;
-//Imprimir "El valor de la suma de los positivos es : ", sumaPositivo;
-//Imprimir "La cantidad de los numeros positivos son : ", cantidadPositivo;
-//Imprimir "La cantidad de los numeros negativos son : ", cantidadNegativo;
-//Imprimir "La diferencia entre numeros positivos y negativos es de : ", diferenciaEntreNumeros;
-//Imprimir "El numero maximo ingresado es : ", numeroMaximo;
-//Imprimir "La cantidad de numeros pares es : ", cantidadPares;
-//Imprimir "La cantidad de ceros es : ", cantidadCeros;
-
-
-//FinAlgoritmo
+//i.Diferencia entre positivos y negativos, (positivos-negativos)
+diferenciaNumero = sumaPositivo + sumaNegativo; // le descuento los positivos
